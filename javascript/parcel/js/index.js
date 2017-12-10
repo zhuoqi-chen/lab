96df2ffe4ccb15bf638b66cec192351e6efb8f23
chenzhuoqi <chenzuoqi@patsnap.com>
2017-12-11 00:39:59 +0800

    test https://github.com/parcel-bundler/parcel

---
+++ javascript/parcel/js/index.js
@@ -0,0 +1,4 @@
+import '../css/style.css';
+import {name} from './module.js';
+console.log(name);
+console.log('https://github.com/parcel-bundler/parcel');
---
+++ javascript/parcel/js/module.js
@@ -0,0 +1,4 @@
+export const name='chenzhuoqi'
+export default {
+    name: 'chenzhuoqi'
+}
